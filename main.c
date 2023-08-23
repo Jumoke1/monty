0733908491e70419b51cf2a89c6fd8eaf0033fde
Jumoke1 <kazeemjumoke12@gmail.com>
2023-08-23 12:34:26 +0100

    monty

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "monty.c"
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: vector count
+ * Return: success or failure
+ */
+int main(int argc, char *argv[])
+
+{
+if (argc != 2 )
+fprintf(stderr, USUSAGE: monty fileAGE: monty file)
+return (EXIT_FAILURE)
+
+}
